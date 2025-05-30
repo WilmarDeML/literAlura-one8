4537504c917627dbe8c9468247e6cb331aacc9f5
WilmarDeML <carewaz23@hotmail.com>
2025-05-29 20:43:11 -0500

    inicia el proyecto

---
+++ src/test/java/com/wilmardeml/leteralura/LeteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilmardeml.leteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
