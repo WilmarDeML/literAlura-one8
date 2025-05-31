978ed4704cb892402149f5f2bc29d4ed1739c86f
WilmarDeML <carewaz23@hotmail.com>
2025-05-30 23:33:36 -0500

    agrega entidades Libro y Autor

---
+++ src/main/java/com/wilmardeml/literalura/modelos/Autor.java
@@ -0,0 +1,72 @@
+package com.wilmardeml.literalura.modelos;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String nombre;
+
+    private Integer anioNacimiento;
+    private Integer anioMuerte;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
+    private List<Libro> libros;
+
+    public Autor() {}
+
+    public Autor(DatosAutor datosAutor) {
+        nombre = datosAutor.nombre().trim();
+        anioNacimiento = datosAutor.anioNacimiento();
+        anioMuerte = datosAutor.anioMuerte();
+        libros = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getAnioNacimiento() {
+        return anioNacimiento;
+    }
+
+    public void setAnioNacimiento(Integer anioNacimiento) {
+        this.anioNacimiento = anioNacimiento;
+    }
+
+    public Integer getAnioMuerte() {
+        return anioMuerte;
+    }
+
+    public void setAnioMuerte(Integer anioMuerte) {
+        this.anioMuerte = anioMuerte;
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
+}
---
+++ src/main/java/com/wilmardeml/literalura/modelos/Libro.java
@@ -0,0 +1,66 @@
+package com.wilmardeml.literalura.modelos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Autor autor;
+
+    private int numeroDescargas;
+    private String idioma;
+
+    public Libro() {}
+
+    public Libro(DatosLibro datosLibro) {
+        titulo = datosLibro.titulo();
+        numeroDescargas = datosLibro.numeroDescargas();
+        idioma = datosLibro.idiomas().getFirst();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroDescargas() {
+        return numeroDescargas;
+    }
+
+    public void setNumeroDescargas(int numeroDescargas) {
+        this.numeroDescargas = numeroDescargas;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+}
