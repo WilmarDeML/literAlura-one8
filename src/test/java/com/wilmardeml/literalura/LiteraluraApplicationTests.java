56c45a31feb5b7c6995ab6c1709ca1aca9c5a724
WilmarDeML <carewaz23@hotmail.com>
2025-05-31 00:37:25 -0500

    actualiza nombre

--- src/test/java/com/wilmardeml/literalura/LiteraluraApplicationTests.java
+++ src/test/java/com/wilmardeml/literalura/LiteraluraApplicationTests.java
@@ -1,10 +1,10 @@
-package com.wilmardeml.leteralura;
+package com.wilmardeml.literalura;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LeteraluraApplicationTests {
+class LiteraluraApplicationTests {
 
 	@Test
 	void contextLoads() {
