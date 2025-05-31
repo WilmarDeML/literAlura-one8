7f9345bae2446e51c1ad4aff3f8b176976ef2b79
WilmarDeML <carewaz23@hotmail.com>
2025-05-30 23:26:54 -0500

    agrega record DatosLibro  para capturar los libros de la API

---
+++ src/main/java/com/wilmardeml/literalura/modelos/DatosLibro.java
@@ -0,0 +1,14 @@
+package com.wilmardeml.literalura.modelos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DatosAutor> autores,
+        @JsonAlias("download_count") int numeroDescargas,
+        @JsonAlias("languages") List<String> idiomas
+) {}
